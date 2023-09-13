ff256694fd83a60302b8c1c20304cf61d129fb8e
Aaryan562 <aaryanclever555@gmail.com>
2023-09-13 22:51:15 +0530

    added 2 functionalities: ask to pdf and ask to dataset

---
+++ langchain-ask-csv/main.py
@@ -0,0 +1,33 @@
+from tempfile import NamedTemporaryFile
+import tempfile
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+import os
+os.environ['OPENAI_API_KEY'] = "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"
+
+def main():
+    load_dotenv()
+
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV")
+
+    file = st.file_uploader("upload file", type="csv")
+
+    if file is not None:
+        temp_dir = 'C:/Temp'  # Change this to the desired directory path
+        os.makedirs(temp_dir, exist_ok=True)  # Create the directory if it 
+        with NamedTemporaryFile(mode='w+b', suffix=".csv",dir=temp_dir, delete=False) as f:
+            f.write(file.getvalue())
+            f.flush()
+            llm = OpenAI(temperature=0)
+            user_input = st.text_input("Question here:")
+            agent = create_csv_agent(llm, f.name, verbose=True)
+            if user_input:
+                response = agent.run(user_input)
+                st.write(response)
+
+
+if __name__ == "__main__":
+    main()
